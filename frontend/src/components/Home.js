ebd1eef942440ee5058778433080b1bba3257c62
Hector Toro <asdxdd7@gmail.com>
2024-07-26 07:58:10 -0400

    Final no docker

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Home.css'; // Asegúrate de crear este archivo CSS
+import './Home.css';
 
 const Home = () => {
   return (
@@ -8,6 +8,7 @@ const Home = () => {
       <h1 className="home-title">Bienvenido a MediPrecio</h1>
       <p className="home-subtitle">Tu sitio de confianza para comparar precios de medicamentos.</p>
       <Link to="/buscar" className="btn btn-primary home-button">Buscar Fármacos</Link>
+      <Link to="/farmacias" className="btn btn-secondary home-button">Ver Todas las Farmacias</Link> {/* Nuevo botón */}
     </div>
   );
 };
