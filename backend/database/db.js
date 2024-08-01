163ce99b16b95b03f4533836d3e221d803a8b489
Hector Toro <asdxdd7@gmail.com>
2024-08-01 06:35:41 -0400

    Test 2

--- backend/database/db.js
+++ backend/database/db.js
@@ -3,7 +3,7 @@ import { Sequelize } from "sequelize";
 
 
 /* Se realiza la conexión a la base de datos */
-const db = new Sequelize('database_ma', 'root', '',{
+const db = new Sequelize('database_ma', 'root', 'password',{
     host:'database',
     dialect: 'mysql',
     port: 3306 
